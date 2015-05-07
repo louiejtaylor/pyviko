4a7b56fd6ac07f1fa9a55d68fcdc317bd608c79a
louiejtaylor <louie.j.taylor@gmail.com>
2015-05-07 12:42:18 -0400

    Fix tabs, minor updas

--- pyviko/mutation.py
+++ pyviko/mutation.py
@@ -1,84 +1,95 @@
 from pyviko import core, restriction
 
 class OverGene:
-    
-    # dummy variables
-    frame = 1
-    startNucleotide = 0
-    sequence = ''
-    overAAs = ''
-    
-    def __init__(self, startNt, seq, frameOver = 1):
-        self.startNucleotide = startNt
-        self.sequence = seq
-        self.frame = frameOver
-        self.overAAs = core.translate(core.findOverprintedGene(seq, startNt-1, frameOver))
+	# dummy variables
+	frame = 1
+	startNucleotide = 0
+	sequence = ''
+	overAAs = ''
+	
+	def __init__(self, startNt, seq, frameOver = 1):
+		self.startNucleotide = startNt
+		self.sequence = seq
+		self.frame = frameOver
+		self.overAAs = core.translate(core.findOverprintedGene(seq, startNt-1, frameOver))
 
 class Mutant:
-    
-    # dummy variables
-    seq = ''
-    nMut = 1
-    isOverGene = False
-    mutants = [] #will hold tuple?!
-    
-    def __init__(self, sequence, numMutations = 1):
-        self.seq = sequence
-        self.nMut = numMutations
-        
-    def overGene(self, startNt, overFrame = 1):
-        self.overGene = OverGene(startNt, self.seq, overFrame)
-        self.isOverGene = True
-        
-    def findMutants(self, rSiteLength = 6, rSites = restriction.restrictionSites):
-        if rSiteLength == 'all':
-            x = 7
-        elif 5 <= rSiteLength and rSiteLength <= 10:
-            x = 8
-        else:
-            #throw error
-            x = 9
-    
+	
+	# dummy variables
+	seq = ''
+	nMut = 1
+	isOverGene = False
+	mutants = [] #will hold tuple?!
+	
+	def __init__(self, sequence, numMutations = 1):
+		self.seq = sequence
+		self.nMut = numMutations
+		
+	def overGene(self, startNt, overFrame = 1):
+	'''
+	Adds the overprinted gene to the current sequence object.
+	'''
+		self.overGene = OverGene(startNt, self.seq, overFrame)
+		self.isOverGene = True
+		
+	def findMutants(self, rSiteLength = 6, rSites = restriction.restrictionSites):
+	'''
+	Returns a list of mutants that add a premature stop codon 
+	(or mutate the start codon) without changing the overprinted 
+	gene, and which add or remove a restriction site.
+	'''
+		if rSiteLength == 'all':
+			x = 7
+		elif 5 <= rSiteLength and rSiteLength <= 10:
+			x = 8
+		else:
+			#throw error
+			x = 9
+	
 def findPossibleStopCodons(codons, n):
-    
-    if codons[-1] in core.stopCodons: 
-        codons = codons[:-1] #remove c-terminal stop codon
-    
-    almostStopCodons = {}
-    #build dict of codons that can be mutated to a stop codon 
-    
-    for c in core.stopCodons:
-        for i in range(0,3):
-            for nt in 'ACTG':
-                if c[:i]+nt+c[i+1:] not in core.stopCodons:
-                    try:
-                        almostStopCodons[c[:i]+nt+c[i+1:]].append(c)
-                    except KeyError:
-                        almostStopCodons[c[:i]+nt+c[i+1:]] = [c]
-    
-    if n == 2:
-        for c in almostStopCodons.keys():
-            for i in range(0,3):
-                for nt in 'ACTG':
-                    if c[:i]+nt+c[i+1:] not in core.stopCodons:
-                        try:
-                            almostStopCodons[c[:i]+nt+c[i+1:]] += almostStopCodons[c]
-                        except KeyError:
-                             almostStopCodons[c[:i]+nt+c[i+1:]] = almostStopCodons[c]
-        for i in almostStopCodons.keys():
-            almostStopCodons[i] = list(set(almostStopCodons[i]))
-    
-    
-    # creates a list of tuples of the form (index, ['list', 'of', 'stop', 'codons']) to be further pre-processed    
-    preMatches = [(i, almostStopCodons[codons[i]]) for i in range(0,len(codons)) if codons[i] in almostStopCodons.keys()]
-    
-    matches = []  
-    
-    # further processing to create actual tuples (index, 'codon')    
-    for m in preMatches:
-        for codon in m[1]:
-            matches.append((m[0],codon))
-            
-    return matches
+	'''
+	Given a list of `codons`, finds individual codons that
+	can be mutated to a stop codon given `n` mutations. Returns 
+	a list of tuples of the form `(index, 'codon')` where 
+	'''
+	if codons[-1] in core.stopCodons: 
+		codons = codons[:-1] #remove c-terminal stop codon
+	
+	almostStopCodons = {}
+	#build dict of codons that can be mutated to a stop codon 
+	
+	for c in core.stopCodons:
+		for i in range(0,3):
+			for nt in 'ACTG':
+				if c[:i]+nt+c[i+1:] not in core.stopCodons:
+					try:
+						almostStopCodons[c[:i]+nt+c[i+1:]].append(c)
+					except KeyError:
+						almostStopCodons[c[:i]+nt+c[i+1:]] = [c]
+	
+	if n == 2:
+		for c in almostStopCodons.keys():
+			for i in range(0,3):
+				for nt in 'ACTG':
+					if c[:i]+nt+c[i+1:] not in core.stopCodons:
+						try:
+							almostStopCodons[c[:i]+nt+c[i+1:]] += almostStopCodons[c]
+						except KeyError:
+							 almostStopCodons[c[:i]+nt+c[i+1:]] = almostStopCodons[c]
+		for i in almostStopCodons.keys():
+			almostStopCodons[i] = list(set(almostStopCodons[i]))
+	
+	
+	# creates a list of tuples of the form (index, ['list', 'of', 'stop', 'codons']) to be further pre-processed	
+	preMatches = [(i, almostStopCodons[codons[i]]) for i in range(0,len(codons)) if codons[i] in almostStopCodons.keys()]
+	
+	matches = []  
+	
+	# further processing to create actual tuples (index, 'codon')	
+	for m in preMatches:
+		for codon in m[1]:
+			matches.append((m[0],codon))
+			
+	return matches
 
 # pyviko.mutation.Mutant('ATGGCGCGGCTAAGGGCCTAA')
--- pyviko/restriction.py
+++ pyviko/restriction.py
@@ -1,73 +1,73 @@
 restrictionSites = {'GCGATCGC': ['AsiSI'], 'ACATGT': ['PciI'], 'TCCGGA': ['BspEI'], 'GGCGCGCC': ['AscI'], 'AGTACT': ['ScaI'], 'ACGCGT': ['MluI'], 'CTCAAG': ['BpuEI'], 'GCCGAG': ['NmeAIII'], 'GGTACC': ['KpnI', 'Acc65I'], 'GGCGGA': ['EciI'], 'CAGCTG': ['PvuII'], 'TTCGAA': ['BstBI'], 'CCCAGT': ['BmrI'], 'GATATC': ['EcoRV'], 'CTCCTC': ['BseRI'], 'CTTGAG': ['BpuEI'], 'CTGCAG': ['PstI'], 'CTGAAG': ['AcuI'], 'CTGCAC': ['BsgI'], 'CATCGC': ['BtgZI'], 'GTATCC': ['BciVI'], 'CCGCGG': ['SacII'], 'GATCC': ['AlwI', 'Nt.AlwI'], 'ATTTAAAT': ['SwaI'], 'TCACC': ['HphI'], 'CTCGAG': ['XhoI'], 'GAGTC': ['Nt.BstNBI', 'MlyI', 'PleI'], 'CGGCCG': ['EagI'], 'CTGGAG': ['BpmI'], 'ACGGC': ['BceAI'], 'ACTGG': ['BsrI'], 'GGTCTC': ['BsaI'], 'GATGG': ['BccI'], 'GACTC': ['Nt.BstNBI', 'MlyI', 'PleI'], 'ACTAGT': ['SpeI'], 'TCGCGA': ['NruI'], 'CTTAAG': ['AflII'], 'GCGTC': ['HgaI'], 'CCTAGG': ['AvrII'], 'GCAGGT': ['BspMI'], 'ATTAAT': ['AseI'], 'CCTTC': ['HpyAV'], 'GCTGGG': ['BseYI'], 'CAATTG': ['MfeI'], 'AGATCT': ['BglII'], 'ACTGGG': ['BmrI'], 'AGCGCT': ['AfeI'], 'CTCGGC': ['NmeAIII'], 'GCTAGC': ['BmtI', 'NheI'], 'CATATG': ['NdeI'], 'TCATGA': ['BspHI'], 'ACCTGC': ['BspMI'], 'GCTCTTC': ['Nt.BspQI', 'SapI'], 'CATTGC': ['BsrDI', 'Nb.BsrDI'], 'GTGCAC': ['ApaLI'], 'GTGCAG': ['BsgI'], 'CACGAG': ['BssSI'], 'CTGAG': ['BspCNI'], 'GAAGG': ['HpyAV'], 'GAAGA': ['MboII'], 'ACCGGT': ['AgeI'], 'GGCGCC': ['KasI', 'SfoI', 'NarI', 'PluTI'], 'GTATAC': ['BstZ17I'], 'GTTAAC': ['HpaI'], 'GACGC': ['HgaI'], 'ATCGAT': ['ClaI'], 'GAAGAG': ['EarI'], 'GGATCC': ['BamHI'], 'GAAGAC': ['BbsI'], 'CGTCTC': ['BsmBI'], 'GAGACC': ['BsaI'], 'CTCTTC': ['EarI'], 'TACGTA': ['SnaBI'], 'CCTCAGC': ['Nb.BbvCI', 'Nt.BbvCI', 'BbvCI'], 'CACTGC': ['BtsI', 'Bts-alpha-I'], 'GTCCC': ['BsmFI'], 'GCATC': ['SfaNI'], 'AAGCTT': ['HindIII'], 'TCCGCC': ['EciI'], 'GCGGG': ['FauI'], 'CTCGTG': ['BssSI'], 'GCCGT': ['BceAI'], 'CCCGGG': ['SmaI', 'XmaI'], 'GAGGAG': ['BseRI'], 'TGATCA': ['BclI'], 'GACGTC': ['AatII', 'ZraI'], 'GCAGC': ['BbvI'], 'TTTAAA': ['DraI'], 'GACGTG': ['BmgBI'], 'CATCC': ['BtsCI', 'FokI'], 'CTCCAG': ['BpmI'], 'AACGTT': ['AclI'], 'CCCGC': ['FauI'], 'GTTTAAAC': ['PmeI'], 'CCCAGC': ['BseYI'], 'TCTTC': ['MboII'], 'CCATGG': ['NcoI'], 'GAGAC': ['Nt.BsmAI', 'BsmAI'], 'CCATC': ['BccI'], 'CCGCTC': ['BsrBI'], 'GAAGAGC': ['Nt.BspQI', 'SapI'], 'CTGCTG': ['EcoP15I'], 'GGTGA': ['HphI'], 'GCGCGC': ['BssHII'], 'GGGAC': ['BsmFI'], 'GGATAC': ['BciVI'], 'GCATGC': ['SphI'], 'GTCGAC': ['SalI'], 'GCAGTG': ['BtsI', 'Bts-alpha-I'], 'TCTAGA': ['XbaI'], 'GAGCTC': ['SacI', 'Eco53kI'], 'GCTGAGG': ['Nb.BbvCI', 'Nt.BbvCI', 'BbvCI'], 'CGTACG': ['BsiWI'], 'GCATTC': ['Nb.BsmI', 'BsmI'], 'GAATGC': ['Nb.BsmI', 'BsmI'], 'CAGCAG': ['EcoP15I'], 'GGATG': ['BtsCI', 'FokI'], 'GGATC': ['AlwI', 'Nt.AlwI'], 'TGCGCA': ['FspI'], 'GCGGCCGC': ['NotI'], 'TGTACA': ['BsrGI'], 'GCCGGC': ['NaeI', 'NgoMIV'], 'TGGCCA': ['MscI'], 'CTTCAG': ['AcuI'], 'GAGCGG': ['BsrBI'], 'TTAATTAA': ['PacI'], 'GCTGC': ['BbvI'], 'ATGCAT': ['NsiI'], 'GATGC': ['SfaNI'], 'CTCAG': ['BspCNI'], 'AATATT': ['SspI'], 'CACGTG': ['PmlI'], 'CACGTC': ['BmgBI'], 'GAGACG': ['BsmBI'], 'CGATCG': ['PvuI'], 'GGGCCC': ['PspOMI', 'ApaI'], 'CCAGT': ['BsrI'], 'GGCCGGCC': ['FseI'], 'GCAATG': ['BsrDI', 'Nb.BsrDI'], 'CCTGCAGG': ['SbfI'], 'GTCTTC': ['BbsI'], 'GAATTC': ['EcoRI'], 'TTATAA': ['PsiI'], 'GTCTC': ['Nt.BsmAI', 'BsmAI'], 'GCGATG': ['BtgZI'], 'AGGCCT': ['StuI'], 'GTTGGA': ['MmeI'], 'GTCGGA': ['MmeI'], 'TCCAAC': ['MmeI'], 'TCCGAC': ['MmeI']}
 
 try:
-    import regex as re
+	import regex as re
 except ImportError:
-    import re
-    print "reeee"
+	print "To support overlapping restriction sites, please update to the new regex module."
+	import re
 
 nucleotideMatrix = {'R':['A','G'],
-                    'Y':['C','T'],
-                    'W':['A','T'],
-                    'S':['C','G'],
-                    'M':['A','C'],
-                    'K':['G','T'],
-                    'B':['C','G','T'],
-                    'D':['A','G','T'],              
-                    'H':['A','C','T'],
-                    'V':['A','C','G'],
-                    'N':['A','C','G','T']}
+					'Y':['C','T'],
+					'W':['A','T'],
+					'S':['C','G'],
+					'M':['A','C'],
+					'K':['G','T'],
+					'B':['C','G','T'],
+					'D':['A','G','T'],			  
+					'H':['A','C','T'],
+					'V':['A','C','G'],
+					'N':['A','C','G','T']}
 
 def findNonRegexEnzymeSites(site):
-    '''
-    Builds a list of sequences that correspond to a given 
-    restriction enzyme recognition site (tree walking).
-    '''
-    possible_seqs = ['']
-    for nt in site:
-        if nt in 'ACGT':
-            for i in xrange(len(possible_seqs)):
-                possible_seqs[i] = possible_seqs[i] + nt
-        else:
-            placeholder = []
-            for seq in possible_seqs:
-                for possibility in nucleotideMatrix[nt]:
-                    placeholder.append(seq+possibility)
-            possible_seqs = [ss for ss in placeholder]
-    return possible_seqs
-    
+	'''
+	Builds a list of sequences that correspond to a given 
+	restriction enzyme recognition site (tree walking).
+	'''
+	possible_seqs = ['']
+	for nt in site:
+		if nt in 'ACGT':
+			for i in xrange(len(possible_seqs)):
+				possible_seqs[i] = possible_seqs[i] + nt
+		else:
+			placeholder = []
+			for seq in possible_seqs:
+				for possibility in nucleotideMatrix[nt]:
+					placeholder.append(seq+possibility)
+			possible_seqs = [ss for ss in placeholder]
+	return possible_seqs
+	
 def findEnzymeSiteRegex(site):
-    '''
-    Returns a naive regular expression for a given
-    restriction site.
-    '''
-    r_site = ''
-    for nt in site:
-        if nt in 'ACGT':
-            r_site += nt
-        else:
-            r_site += '['
-            try:
-                for m in nucleotideMatrix[nt]:
-                    r_site += m
-            except KeyError:
-                print "Unknown nucleotide '" + nt  + "' encountered."
-            r_site += ']'
-    return r_site
+	'''
+	Returns a naive regular expression for a given
+	restriction site.
+	'''
+	r_site = ''
+	for nt in site:
+		if nt in 'ACGT':
+			r_site += nt
+		else:
+			r_site += '['
+			try:
+				for m in nucleotideMatrix[nt]:
+					r_site += m
+			except KeyError:
+				print "Unknown nucleotide '" + nt  + "' encountered."
+			r_site += ']'
+	return r_site
 
 def findNcutters(seq, n):
-    '''
-    Find restriction sites of length `n` in a sequence `seq`
-    in `O(n*m)` where n is the sequence length and m is the
-    number of restriction enzymes.
-    '''
-    n_mers = []
-    for i in range(0, len(seq) - (n-1)):
-        n_mers.append((i, seq[i:i+n]))
-    actualSites = []
-    for s in n_mers:
-        if s[1] in restrictionSites.keys():
-            actualSites.append((s[0],restrictionSites[s[1]]))
-            
-    # Limitation: assumes dict restrictionSites is in non-regex form (could be very large)
-    return actualSites
+	'''
+	Find restriction sites of length `n` in a sequence `seq`
+	in `O(n*m)` where n is the sequence length and m is the
+	number of restriction enzymes.
+	'''
+	n_mers = []
+	for i in range(0, len(seq) - (n-1)):
+		n_mers.append((i, seq[i:i+n]))
+	actualSites = []
+	for s in n_mers:
+		if s[1] in restrictionSites.keys():
+			actualSites.append((s[0],restrictionSites[s[1]]))
+			
+	# Limitation: assumes dict restrictionSites is in non-regex form (could be very large)
+	return actualSites
