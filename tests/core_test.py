ec3e63039028500907226225d10e5f53b4179aca
louiejtaylor <louie.j.taylor@gmail.com>
2020-09-20 11:52:56 -0400

    informative names for more tests

--- tests/core_test.py
+++ tests/core_test.py
@@ -38,28 +38,28 @@ class coreTestCase(unittest.TestCase):
 		self.assertRaises(TypeError, codonify(self.invalid_input))
 
 	# seqify 
-	def test_seqify_1(self):
+	def test_codon_to_seq(self):
 		"""Tests sequify takes cod -> seq"""
 		self.assertEqual(seqify(self.cod), self.seq)
-	def test_seqify_2(self):
+	def test_seq_to_seq(self):
 		"""Tests sequify takes seq -> seq"""
 		self.assertEqual(seqify(self.seq), self.seq)
-	def test_seqify_3(self):
+	def test_seqify_invalid(self):
 		"""Tests sequify's handling of invalid input types"""
 		self.assertRaises(TypeError, seqify(self.invalid_input))
 	
 	# translate
-	def test_translate_1(self):
+	def test_translate_codon(self):
 		"""Tests translate takes cod -> aa"""
 		self.assertEqual(translate(self.cod), self.trans)
-	def test_translate_2(self):
+	def test_invalid_codon(self):
 		"""Tests translate rasies SequenceError for invalid codon"""
 		self.assertRaises(SequenceError, translate(self.cod_invalid))
-	def test_translate_3(self):
+	def test_start_codon_warning(self):
 		"""Tests translate warns user if no start codon is found"""
 	        self.assertWarns(Warning, translate(self.cod_no_start))
                 self.assertEqual(translate(self.cod_no_start), self.trans_no_start)
-	def test_translate_4(self):
+	def test_stop_codon_warning(self):
 		"""Tests translate warns user if no stop codon is found"""
                 self.assertWarns(Warning, translate(self.cod_no_stop))
                 self.assertEqual(translate(self.cod_no_stop), self.trans_no_stop)
