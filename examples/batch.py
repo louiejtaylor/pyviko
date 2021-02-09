c4ffdd0dc60d4689d224cdf66d277cc3573bec2d
louiejtaylor <louie.j.taylor@gmail.com>
2021-02-09 09:01:50 -0500

    update example

--- examples/batch.py
+++ examples/batch.py
@@ -8,9 +8,9 @@ if __name__ == '__main__':
 	toKO  = ['ATGAAACCCTTTGGGAAACCCGGGTAA']
         # benchmarking overlapping in the reverse frame
 	for i in range(len(toKO))[:1]:
-		m = mutation.Mutant(toKO[i][1],numMutations=1,regEx=True)
-		m.setOverGene(overSeq = ovr[i][1])
-		print(m.findMutants(ignoreRxSites = False, rSiteLength='all')[:5])
-		print(m.findMutants()[:5])
+		m = mutation.Mutant(toKO[i][1],num_mutations=1,regex=True)
+		m.set_over_gene(over_seq = ovr[i][1])
+		print(m.find_mutants(ignore_rx_sites = False, r_site_length='all')[:5])
+		print(m.find_mutants()[:5])
 		print("done "+str(i))
 	
