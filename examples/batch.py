ce31d51c4cdbcb54b9346508d52388ec4ef2e125
louiejtaylor <louiejtaylor@users.noreply.github.com>
2023-04-01 15:43:48 -0400

    cleanup batch

--- examples/batch.py
+++ examples/batch.py
@@ -1,10 +1,9 @@
 if __name__ == '__main__':
-	
 	from pyviko import core, mutation, restriction	
 	# testing RC search
 	ovr=['ATGATTACCCGGGTTTCCCAAAGGGTTTCATCCTAA']
 	#         TTACCCGGGTTTCCCAAAGGGTTTCAT bounds of gene in the minus dir`
-	to_ko  = ['ATGAAACCCTTTGGGAAACCCGGGTAA']
+	to_ko = ['ATGAAACCCTTTGGGAAACCCGGGTAA']
         # benchmarking overlapping in the reverse frame
 	for i in range(len(to_ko))[:1]:
 		m = mutation.Mutant(to_ko[i][1],num_mutations=1,regex=True)
@@ -12,4 +11,3 @@ if __name__ == '__main__':
 		print(m.find_mutants(ignore_rx_sites = False, r_site_length='all')[:5])
 		print(m.find_mutants()[:5])
 		print("done "+str(i))
-	
