db0a3ce66abf2fc908390174d26042676dac8b1e
louiejtaylor <louiejtaylor@users.noreply.github.com>
2023-04-18 11:49:35 -0400

    move batch.py to tests--to integrate

---
+++ tests/mutation_test.py
@@ -0,0 +1,12 @@
+from pyviko import core, mutation, restriction	
+# testing RC search
+ovr=['ATGATTACCCGGGTTTCCCAAAGGGTTTCATCCTAA']
+#         TTACCCGGGTTTCCCAAAGGGTTTCAT bounds of gene in the minus dir`
+to_ko = ['ATGAAACCCTTTGGGAAACCCGGGTAA']
+# benchmarking overlapping in the reverse frame
+for i in range(len(to_ko))[:1]:
+	m = mutation.Mutant(to_ko[i][1],num_mutations=1,regex=True)
+	m.set_over_gene(over_seq = ovr[i][1])
+	print(m.find_mutants(ignore_rx_sites = False, r_site_length='all')[:5])
+	print(m.find_mutants()[:5])
+	print("done "+str(i))
