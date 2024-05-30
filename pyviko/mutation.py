6118019e470ace70fcb2bfffb44a9c5268d4b726
louiejtaylor <louiejtaylor@users.noreply.github.com>
2024-05-30 17:53:16 -0400

    update documentation

--- pyviko/mutation.py
+++ pyviko/mutation.py
@@ -9,8 +9,8 @@ class OverGene:
 	start_nucleotide_index = -1
 	gene_sequence = ''
 	# pre and post sequence used when mutating OverGene (to translate the other frame)
-	self.pre_sequence = '' 
-	self.post_sequence = ''
+	pre_sequence = '' 
+	post_sequence = ''
 	over_aas = ''
 
 	def __init__(self, seq, over_seq = None, start_nt_index= None, frame_over = 1):
